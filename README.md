757b2b230496fa11c93721a0bc9a83116707edf0
snsvrno <snsvrno@tuta.io>
2018-11-28 05:26:25 -0700

    moves some documentation from README.md to code comments

--- README.md
+++ README.md
@@ -39,10 +39,12 @@ Add ***Settingsfile-rs*** to your `Cargo.toml` file.
 settingsfile = "^0.2"
 ```
 
-Then create a dummy struct and implement the `Clone` + `Format` trait and then create a new `File`, in most cases you can use `#[derive(Clone)]` for `Clone`.
+Then create a struct and implement the `Clone` + `Format` trait and then create a new `File`, in most cases you can use `#[derive(Clone)]` for `Clone`. This struct is just a container for how you want the physical settings file to be formated.
 
 ```rust
 extern crate settingsfile;
+use settingsfile::Settings;
+use settingsfile::Format;
 // ...
 
 struct Configuration { }
@@ -88,93 +90,3 @@ if let Ok(user_tree) = settings.get_value("user") {
 ``` 
 
 One note is that these are _copies_ so manipulating content from `get_value` will not work. You will need to use `set_value` to do any permanent manipulation.
-
-## Types
-
-#### `pub type PartsPackage = HashMap<String,Type>`
-
-A convience type that is used to shorten the required return type for the `Format` trait implemnetations. This does not need to be used by the users of this library.
-
-## Traits
-
-***Settingsfile-rs*** requires an empty struct with the `Format` trait implemented in order to create a `Settingsfile::File`.
-
-### Required Functions
-
-#### `filename()`
-
-```rust
-fn filename(&self) -> String
-```
-
-Should return the file name of the configuration file; ~/.application_name/***file_name***.extension
-
-The entire name + extension can be used here as well if you don't want to use the [extension()](#extension) function.
-
-####  `folder()`
-
-```rust
-fn folder(&self) -> String
-```
-
-Should return the folder name of the configuration file with respect to the %user_directory%; ~/.***application_name***/file_name.extension
-
-The '.' isn't automatically added, and you would need to include it if desired. If you want to nest into folders, you can do that too using either '/' or '\\'; it will be converted and parsed per platform.
-
-#### `to_string()`
-
-```rust
-fn to_string<T>(&self, object:T) -> Result<String,Error> 
-  where T : settingsfile::SupportedType + serde::de::Serialize
-```
-
-Returns the seralized form of the passed in `object`. Because this uses ***Serde.rs*** the `object` must have the `serde::de::Serialize` trait, and must also implement the `settingsfile::SupportedType` trait.
-
-Typically this is just a wrapped passthrough to the serde libray you are using. Example using [toml-rs](https://github.com/alexcrichton/toml-rs):
-
-```rust
-fn to_string<T:?Sized>(&self,object:&T) -> Result<String,Error>
-  where T : settingsfile::SupportedType + serde::ser::Serialize,
-{
-  match toml::ser::to_string(object) {
-    Ok(string) => Ok(string),
-    Err(error) => Err(Error::Error(error.to_string()))
-  }
-}
-```
-
-#### `from_str()`
-
-```rust
-fn from_str<T>(&self, buffer:&str) -> Result<PartsPackage,Error>
-  where T : Clone + settingsfile::Format
-```
-
-Returns a deserialized form of a string buffer into a Rust object.
-
-Typically this is just a wrapped passthrough to the serde library you are using. Example using [toml-rs](https://github.com/alexcrichton/toml-rs):
-
-```rust
-fn from_str<T>(&self,buffer:&str) -> Result<PartsPackage,Error>
-  where T : Format + Clone
-{
-let result : Result<PartsPackage,toml::de::Error> = toml::from_str(&buffer);
-  match result {
-    Ok(result) => Ok(result),
-    Err(error) => Err(Error::Error(error.to_string())),
-  }
-}
-```
-
-
-### Optional Functions
-
-#### `extension()`
-
-```rust
-fn extension(&self) -> Option<String>
-```
-
-Should return the extension of the configuration file; ~/.application_name/file_name.***extension*** 
-
-If not defined then no extension will be used for the file. ***Settingsfile*** does not assume a file's format by its extension so this is just a matter of user preference.
--- src/traits/format.rs
+++ src/traits/format.rs
@@ -5,21 +5,132 @@ use failure::Error;
 use std::collections::HashMap;
 use serde::ser::Serialize;
 
+/// A convience type that is used to shorten the required return 
+/// type for the `Format` trait implemnetations. This does not need 
+/// to be used by the users of this library, though makes code a little
+/// shorter.
 pub type SettingsRaw = HashMap<String,Type>;
 
 pub trait Format {
 
     // need to be implemneted //////////////////////////////////////
+
+    /// Should return the file name of the configuration file: 
+    /// 
+    /// ~/{.application_name}/***{file_name}***.{extension}
+    /// 
+    /// ```rust,ignore
+    /// fn filename(&self) -> String {
+    ///     "config".to_string()
+    /// }
+    /// ```
+    /// 
+    /// The entire name + extension can be used here as well if 
+    /// you don't want to use the [extension()](#extension) function.
+    /// 
+    /// ```rust,ignore
+    /// fn filename(&self) -> String {
+    ///     "settings.toml".to_string()
+    /// }
+    /// ``` 
     fn filename(&self) -> String;
+
+    /// Should return the folder name of the configuration file with 
+    /// respect to the _%user_directory%_ environmental variable:
+    ///    
+    /// ~/***{.application_name}***/{file_name}.{extension} 
+    /// 
+    /// The '.' isn't automatically added, and you would need to include it if desired. 
+    /// 
+    /// If you want to nest into folders, you can do that too using either '/' or '\\'. 
+    /// It will be converted and parsed per platform.
+    /// 
+    /// ~/***.config/console_app***/config
+    /// 
+    /// ```rust,ignore
+    /// fn folder(&self) -> String {
+    ///     ".config/console_app".to_string()
+    /// }
+    /// ``` 
     fn folder(&self) -> String;
+
+    /// Returns the seralized form of the passed in `object`. 
+    /// Because this uses ***Serde.rs*** the `object` must have the 
+    /// `serde::de::Serialize` trait, and must also implement the 
+    /// `settingsfile::SupportedType` trait.
+    /// 
+    /// Typically this is just a wrapped passthrough to the serde libray you are using. 
+    /// Example using [ron-rs](https://github.com/alexcrichton/ron-rs):
+    /// 
+    /// ```rust,ignore
+    /// fn to_string<T:?Sized>(&self,object:&T) -> Result<String,Error>
+    ///   where T : settingsfile::SupportedType + serde::ser::Serialize,
+    /// {
+    ///   match ron::ser::to_string(object) {
+    ///     Ok(string) => Ok(string),
+    ///     Err(error) => Err(format_err!("{}",error))
+    ///   }
+    /// }
+    /// ```
+    /// 
+    /// You can see a working example in the test in the codebase 
+    /// [testing_with_ron](https://github.com/snsvrno/settingsfile-rs/blob/master/tests/testing_with_ron.rs)
     fn to_string<T>(&self,object:&T) -> Result<String,Error> where T : SupportedType + Serialize;
+    
+    /// the decoding function, will return a deserialized form 
+    /// of a the `Settings` Rust Struct.
+    /// 
+    /// Typically this is a wrapped passthrough to the serde library you are using. 
+    /// Example using [ron-rs](https://github.com/alexcrichton/ron-rs):
+    /// 
+    /// ```rust,ignore
+    /// fn from_str<T>(&self,buffer:&str) -> Result<SettingsRaw,Error>
+    ///   where T : Format + Clone
+    /// {
+    /// let result : Result<SettingsRaw,ron::de::Error> = ron::de::from_str(&buffer);
+    ///   match result {
+    ///     Ok(result) => Ok(result),
+    ///     Err(error) => Err(format_err!("{}",error)),
+    ///   }
+    /// }
+    /// ```
+    /// 
+    /// You can see a working example in the test in the codebase 
+    /// [testing_with_ron](https://github.com/snsvrno/settingsfile-rs/blob/master/tests/testing_with_ron.rs)
     fn from_str<T>(&self,buffer:&str) -> Result<SettingsRaw,Error> where T : Format + Clone;
 
     // have default implemntations ////////////////////////////////
-    fn extension(&self) -> Option<String> { None }
+    fn extension(&self) -> Option<String> {
+        //! option to allow for an extension separate from filename,
+        //! you can always put the extension in the filename if you 
+        //! perfer.
+        //! 
+        //! ~/{.application_name}/{file_name}.{***extension***}
+        //!  
+        //! ```rust,ignore
+        //! fn extension(&self) -> Option<String> {
+        //!     Some("toml".to_string())
+        //! }
+        //! ```
+        //! 
+        //! If not defined then no extension will be used for the file.
+        //! ***Settingsfile*** does not assume a file's format by its
+        //! extension so this is just a matter of user preference.
+         
+        None
+    }
+    fn allow_local_overrides(&self) -> bool { 
+        //! option to allow for local setting files to override the 
+        //! global settings defined in this configuration.
+        
+        true 
+    }
 
     // functions that shouldn't generally need to be implemented //
     fn get_path(&self) -> String {
+        //! will give the correct path depending on what was implemented
+        //! in the configuration
+        
         if let Some(ext) = self.extension() {
             return format!("{}/{}.{}",self.folder(),self.filename(),ext);
         } else {
